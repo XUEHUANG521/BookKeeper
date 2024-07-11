2763bac2f3b83669e392e14683425286a281451b
XUEHUANG521 <75666585+XUEHUANG521@users.noreply.github.com>
2024-07-11 21:34:55 +1000

    update test-env

--- pages/api/test-env.ts
+++ pages/api/test-env.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 export default function handler(req:NextApiRequest, res:NextApiResponse) {
 	res.status(200).json({ databaseUrl: process.env.DATABASE_URL,
-		environment: process.env.environment});
+		environment: process.env.environment,
+		secret: process.env.JWT_SECRET});
   }
   
